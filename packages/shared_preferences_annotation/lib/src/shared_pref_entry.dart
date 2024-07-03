936e596573efe6159124ac0880e03f14f006c7ae
Guillaume Roux <rouxguillaume8@gmail.com>
2024-07-03 20:16:42 +0200

    chore: started implementing a generator

--- packages/shared_preferences_annotation/lib/src/shared_pref_entry.dart
+++ packages/shared_preferences_annotation/lib/src/shared_pref_entry.dart
@@ -1,7 +1,12 @@
 class SharedPrefEntry<T extends Object> {
   const SharedPrefEntry({
     required this.key,
+    this.defaultValue,
   });
 
+  /// Unique key for the entry.
   final String key;
+
+  /// Default value for the entry.
+  final T? defaultValue;
 }
--- packages/shared_preferences_gen/lib/src/shared_preferences_generator.dart
+++ packages/shared_preferences_gen/lib/src/shared_preferences_generator.dart
@@ -1,5 +1,39 @@
+import 'dart:async';
+
+import 'package:analyzer/dart/element/element.dart';
+import 'package:build/build.dart';
+import 'package:shared_preferences_annotation/shared_preferences_annotation.dart';
 import 'package:source_gen/source_gen.dart';
+
+const _annotations = <Type>{
+  SharedPrefEntry,
+};
 
 class SharedPreferencesGenerator extends Generator {
   const SharedPreferencesGenerator();
+
+  TypeChecker get _typeChecker =>
+      TypeChecker.any(_annotations.map((e) => TypeChecker.fromRuntime(e)));
+
+  @override
+  Future<String> generate(LibraryReader library, BuildStep buildStep) async {
+    generateForAnnotation(library);
+
+    // TODO: implement generate
+    return [''].join('\n\n');
+  }
+
+  void generateForAnnotation(LibraryReader library) {
+    for (final annotatedElement in library.annotatedWith(_typeChecker)) {
+      final generatedValue = _generateForAnnotatedElement(
+        annotatedElement.element,
+        annotatedElement.annotation,
+      );
+    }
+  }
+
+  Object _generateForAnnotatedElement(
+    Element element,
+    ConstantReader annotation,
+  ) {}
 }
