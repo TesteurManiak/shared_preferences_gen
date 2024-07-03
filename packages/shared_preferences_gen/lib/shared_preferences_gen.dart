5b65987657bfd907987238110ea2e8845cb653b3
Guillaume Roux <rouxguillaume8@gmail.com>
2024-07-03 19:56:47 +0200

    feat: base generator

--- packages/shared_preferences_gen/lib/shared_preferences_gen.dart
+++ packages/shared_preferences_gen/lib/shared_preferences_gen.dart
@@ -1,3 +1,16 @@
 library shared_preferences_gen;
 
-export 'builder.dart';
+import 'package:build/build.dart';
+import 'package:shared_preferences_gen/src/shared_preferences_generator.dart';
+import 'package:source_gen/source_gen.dart';
+
+/// Supports `package:build_runner` creation and configuration of
+/// `shared_preferences_gen`.
+///
+/// Not meant to be invoked by hand-authored code.
+Builder sharedPreferencesGen(BuilderOptions options) {
+  return SharedPartBuilder(
+    const <Generator>[SharedPreferencesGenerator()],
+    'shared_preferences_gen',
+  );
+}
---
+++ packages/shared_preferences_gen/lib/src/shared_preferences_generator.dart
@@ -0,0 +1,5 @@
+import 'package:source_gen/source_gen.dart';
+
+class SharedPreferencesGenerator extends Generator {
+  const SharedPreferencesGenerator();
+}
