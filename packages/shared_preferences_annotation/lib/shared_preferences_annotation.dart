53a8d59267ba3463820c761266e5aea9f2441722
Guillaume Roux <groux@veson.com>
2024-07-04 11:08:14 +0200

    chore: export type adapters

--- packages/shared_preferences_annotation/lib/shared_preferences_annotation.dart
+++ packages/shared_preferences_annotation/lib/shared_preferences_annotation.dart
@@ -2,3 +2,5 @@ library shared_preferences_annotation;
 
 export 'src/shared_pref_data.dart';
 export 'src/shared_pref_entry.dart';
+export 'src/shared_pref_gen_utils.dart';
+export 'src/shared_pref_type_adapter.dart';
