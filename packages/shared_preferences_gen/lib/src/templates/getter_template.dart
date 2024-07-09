cd3ecf3c9dd1ac6428854d77a503d2f7e9bb74d8
Guillaume Roux <rouxguillaume8@gmail.com>
2024-07-09 18:02:46 +0200

    refactor: use map of shared prefs methods

--- packages/shared_preferences_gen/lib/src/templates/getter_template.dart
+++ packages/shared_preferences_gen/lib/src/templates/getter_template.dart
@@ -1,5 +1,7 @@
 part of 'gen_template.dart';
 
+typedef _SpMethods = ({String spGetter, String spSetter});
+
 class GetterTemplate extends GenTemplate {
   const GetterTemplate({
     required this.key,
@@ -23,23 +25,21 @@ class GetterTemplate extends GenTemplate {
   final String inputType;
   final String outputType;
 
-  ({String getter, String setter}) get sharedPrefMethods {
-    return switch (inputType) {
-      'String' => (getter: 'getString', setter: 'setString'),
-      'int' => (getter: 'getInt', setter: 'setInt'),
-      'double' => (getter: 'getDouble', setter: 'setDouble'),
-      'bool' => (getter: 'getBool', setter: 'setBool'),
-      'List<String>' => (getter: 'getStringList', setter: 'setStringList'),
-      _ => throw StateError('Unsupported type: $inputType'),
-    };
-  }
+  static const _spMethods = <String, _SpMethods>{
+    'String': (spGetter: 'getString', spSetter: 'setString'),
+    'int': (spGetter: 'getInt', spSetter: 'setInt'),
+    'double': (spGetter: 'getDouble', spSetter: 'setDouble'),
+    'bool': (spGetter: 'getBool', spSetter: 'setBool'),
+    'List<String>': (spGetter: 'getStringList', spSetter: 'setStringList'),
+  };
+
+  _SpMethods get _sharedPrefMethods => _spMethods[inputType]!;
 
   @override
   String build() {
-    final (getter: spGetter, setter: spSetter) = sharedPrefMethods;
-    final hasAdapter = adapter != null;
+    final (:spGetter, :spSetter) = _sharedPrefMethods;
     final hasDefault = defaultValue != null || defaultValueAsString != null;
-    final (:getter, :setter) = switch (hasAdapter) {
+    final (:getter, :setter) = switch (adapter != null) {
       true => (
           getter: '(k) => adapter.fromSharedPrefs($spGetter(k))',
           setter: '(k, v) => $spSetter(k, adapter.toSharedPrefs(v))',
